d4ba743bd1243d34ba58fde3829dc202e940175b
Vedant Singhania <vedant.singhania@gmail.com>
2022-07-20 09:37:55 -0700

    messing with the Julia programming language

---
+++ functions.jl
@@ -0,0 +1,5 @@
+function f(x,y)
+	x+y
+end
+
+println(f(2,3))
---
+++ print.jl
@@ -0,0 +1,17 @@
+x = 3
+println(x)
+
+println(pi)
+
+a = 1+2im
+b = 2 - 1im
+println(a-b)
+
+println(6//9)
+
+c = 'x'
+println(typeof(c))
+
+d = "abc"
+println(typeof(d))
+println(d[1])
